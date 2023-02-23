8ebc547f4d9bb6480519aedee0536d86e7714d4c
Pedro Siniscalchi <pedrosinisca@outlook.com.br>
2023-02-23 00:43:35 -0300

    Plot Color Graphic

--- DataLOG.py
+++ DataLOG.py
@@ -1,5 +1,23 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-print("Teste de Data Science")
+# Gerar dados aleatórios
+x = np.random.rand(50)
+y = np.random.rand(50)
+sizes = 1000 * np.random.rand(50)
+colors = np.random.rand(50)
+
+# Criar o gráfico de dispersão
+plt.scatter(x, y, s=sizes, c=colors, alpha=0.5)
+
+# Adicionar rótulos e título
+plt.xlabel('Eixo x')
+plt.ylabel('Eixo y')
+plt.title('Gráfico de dispersão com tamanho e cor personalizados')
+
+# Adicionar barra de cores
+colorbar = plt.colorbar()
+colorbar.set_label('Valor da cor')
+
+# Exibir o gráfico
+plt.show()
